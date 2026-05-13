cff3619b5601f3af9a9dd77d0f71142613fcf440
mostafayoussef10993 <mostafayoussef10993@gmail.com>
2026-05-13 21:36:18 +0300

    ANdroid mainfest

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
 
     // Optional (recommended for analytics/debugging)
     implementation("com.google.firebase:firebase-analytics")
+    // The Regula Core package is large and requires multidex
+    implementation 'androidx.multidex:multidex:2.0.1' 
 }
 
 flutter {
